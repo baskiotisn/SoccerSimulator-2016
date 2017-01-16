c658b5c2cfcbbbadf94d869dc71a541d87669834
baskiotisn <nicolas.baskiotis@lip6.fr>
2017-01-16 15:58:17 +0100

    TME 1

--- soccersimulator/utils.py
+++ soccersimulator/utils.py
@@ -244,6 +244,8 @@ class Vector2D(JSONable):
             nidx = strg.find(")", idx)
         return res
 
+    def __repr__(self):
+        return self.__str__()
     def __str__(self):
         return "(%f,%f)" % (self.x, self.y)
     def to_dict(self):
